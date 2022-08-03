2c5562d7a516c6c162f33e9b15d8dba11d595959
Toni Schmidbauer <toni@stderr.at>
2022-08-03 08:26:04 +0200

    update

---
+++ 03_first_steps/FloatAndDouble/src/at/stderr/masterclass/Main.java
@@ -0,0 +1,24 @@
+package at.stderr.masterclass;
+
+public class Main {
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMindoubleValue = Double.MIN_VALUE;
+        double myMaxdoubleValue = Double.MAX_VALUE;
+        System.out.println("double minimum value = " + myMindoubleValue);
+        System.out.println("double maximum value = " + myMaxdoubleValue);
+
+        int myIntValue = 5;
+        // not recommended
+        // float myFloatValue = (float) 5.25;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
+
+
+    }
+}
