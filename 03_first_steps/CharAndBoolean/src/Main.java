7cbd91561b2c57b56b62fed1eb96d16ce23c5764
Toni Schmidbauer <toni@stderr.at>
2022-08-08 10:27:59 +0200

    update

---
+++ 03_first_steps/CharAndBoolean/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyright = '\u00A9';
+        System.out.println(myCopyright);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
--- 03_first_steps/FloatAndDouble/src/at/stderr/masterclass/Main.java
+++ 03_first_steps/FloatAndDouble/src/at/stderr/masterclass/Main.java
@@ -12,14 +12,23 @@ public class Main {
         System.out.println("double minimum value = " + myMindoubleValue);
         System.out.println("double maximum value = " + myMaxdoubleValue);
 
-        int myIntValue = 5;
+        int myIntValue = 5 / 3;
         // not recommended
         // float myFloatValue = (float) 5.25;
-        float myFloatValue = 5f;
-        double myDoubleValue = 5d;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
 
         System.out.println("MyIntValue=" + myIntValue);
         System.out.println("MyFloatValue=" + myFloatValue);
         System.out.println("MyDoubleValue=" + myDoubleValue);
+
+        double myPound = 200d;
+        double myKilogram = myPound * 0.45359237d;
+        System.out.println(myPound + " pound(s) == " + myKilogram + " kilogram(s)");
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.1_415_927d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
     }
 }
