635b6945125cf3119801cbd077c6a683236943c6
Toni Schmidbauer <toni@stderr.at>
2022-08-12 16:20:02 +0200

    section 04

---
+++ 04_Expressions_Statements_Methods/IfKeywordAndCodeBlocks/src/main/java/at/stderr/IfKeywordAndCodeBlocks/Main.java
@@ -0,0 +1,26 @@
+package at.stderr.IfKeywordAndCodeBlocks;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score less 5000");
+        } else if (score < 1000) {
+            System.out.println("Your score was less then 1000");
+        } else {
+            System.out.println("Got here");
+        }
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score is " + finalScore);
+        }
+    }
+}
---
+++ 04_Expressions_Statements_Methods/StatementsWhitespaceAndIndenting/src/main/java/at/stderr/StatementsWhitespaceAndIndenting/Main.java
@@ -0,0 +1,32 @@
+package at.stderr.StatementsWhitespaceAndIndenting;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) {
+        int     myVariable
+                =
+                50
+                ;
+        myVariable   ++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is " +
+                "another" +
+                "test");
+
+        int anotherVariable = 5; myVariable--; System.out.println("This is another one");
+
+        System
+                .
+                out
+                .
+                println
+                ("test");
+
+
+    }
+}
