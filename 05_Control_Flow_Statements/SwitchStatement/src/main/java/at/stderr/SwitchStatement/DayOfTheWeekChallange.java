f7701d136ad0281971cb1dda6d7b874d55ebfd7d
Toni Schmidbauer <toni@stderr.at>
2022-09-19 17:47:26 +0200

    import 05 exercises

---
+++ 05_Control_Flow_Statements/SwitchStatement/src/main/java/at/stderr/SwitchStatement/DayOfTheWeekChallange.java
@@ -0,0 +1,32 @@
+package at.stderr.SwitchStatement;
+
+public class DayOfTheWeekChallange {
+
+    public static void printDayOfTheWeek(int day) {
+        switch(day) {
+            case 0:
+                System.out.println("Monday");
+                break;
+            case 1:
+                System.out.println("Tuesday");
+                break;
+            case 2:
+                System.out.println("Wednesday");
+                break;
+            case 3:
+                System.out.println("Thursday");
+                break;
+            case 4:
+                System.out.println("Friday");
+                break;
+            case 5:
+                System.out.println("Saturday");
+                break;
+            case 6:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
--- 05_Control_Flow_Statements/SwitchStatement/src/main/java/at/stderr/SwitchStatement/Main.java
+++ 05_Control_Flow_Statements/SwitchStatement/src/main/java/at/stderr/SwitchStatement/Main.java
@@ -57,5 +57,8 @@ public class Main
                 break;
         }
 
+        DayOfTheWeekChallange.printDayOfTheWeek(0);
+        DayOfTheWeekChallange.printDayOfTheWeek(8);
+
     }
 }
