f05c2723ecd58b2c7d759a46e138c81f0a1c41ab
Toni Schmidbauer <toni@stderr.at>
2022-10-15 11:22:28 +0200

    exercises and started constructor section

---
+++ 06_Classes_Constructors_Inheritance/Constructors/src/main/java/at/stderr/Constructors/Main.java
@@ -0,0 +1,12 @@
+package at.stderr.Constructors;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ 06_Classes_Constructors_Inheritance/Exercises/src/main/java/at/stderr/Exercises/Main.java
@@ -0,0 +1,40 @@
+package at.stderr.Exercises;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) {
+        // testSimpleCalculator();
+        testPersion();
+    }
+
+    private static void testSimpleCalculator() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+    }
+
+    private static void testPersion() {
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+    }
+}
