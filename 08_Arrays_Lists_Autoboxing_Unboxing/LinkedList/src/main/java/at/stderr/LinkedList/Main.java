108bac37054631c0c544fe6dea1b03a4fea7f151
Toni Schmidbauer <toni@stderr.at>
2022-11-29 18:32:19 +0100

    work on linkedlists

--- 08_Arrays_Lists_Autoboxing_Unboxing/LinkedList/src/main/java/at/stderr/LinkedList/Main.java
+++ 08_Arrays_Lists_Autoboxing_Unboxing/LinkedList/src/main/java/at/stderr/LinkedList/Main.java
@@ -1,5 +1,8 @@
 package at.stderr.LinkedList;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -7,6 +10,25 @@ package at.stderr.LinkedList;
 public class Main 
 {
     public static void main( String[] args ) {
+        Customer customer = new Customer("Tim", 54.96);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);;
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
 
+        var intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for ( int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1,2);
+
+        for ( int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
     }
 }
