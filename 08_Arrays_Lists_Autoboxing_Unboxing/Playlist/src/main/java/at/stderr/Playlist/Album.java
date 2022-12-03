3d8efc7e2aece0a442c73d86bb1b0a95f9c6f0c4
Toni Schmidbauer <toni@stderr.at>
2022-12-03 16:25:36 +0100

    started playlist exercise

---
+++ 08_Arrays_Lists_Autoboxing_Unboxing/Playlist/src/main/java/at/stderr/Playlist/Album.java
@@ -0,0 +1,42 @@
+package at.stderr.Playlist;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) != null) {
+            return false;
+        }
+
+        songs.add(new Song(title, duration));
+        return true;
+    }
+
+    public boolean addToPlaylist(int track, LinkedList<Song> playlist) {
+        return true;
+    }
+
+    private Song findSong(String title) {
+        for(Song s : songs) {
+            if (s.getTitle().equals(title)) return s;
+        }
+
+        // XXX: should return an UNKNOWN song?
+        return null;
+    }
+
+
+}
---
+++ 08_Arrays_Lists_Autoboxing_Unboxing/Playlist/src/main/java/at/stderr/Playlist/Main.java
@@ -0,0 +1,12 @@
+package at.stderr.Playlist;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) {
+
+    }
+}
