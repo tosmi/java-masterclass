178a62c05e7418030f45797267afb6445869be82
Toni Schmidbauer <toni@stderr.at>
2022-12-31 12:47:34 +0100

    reworked section 7

---
+++ 07_Composition_Encapsulation_Polymorphism/Packages/src/main/java/at/stderr/Packages/Main.java
@@ -0,0 +1,17 @@
+package at.stderr.Packages;
+
+
+import com.abc.first.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) {
+        // com.abc.first.Item firstItem = new com.abc.first.Item("Burger");
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+}
--- 07_Composition_Encapsulation_Polymorphism/Polymorphism/src/main/java/at/stderr/Polymorphism/Main.java
+++ 07_Composition_Encapsulation_Polymorphism/Polymorphism/src/main/java/at/stderr/Polymorphism/Main.java
@@ -1,76 +1,26 @@
 package at.stderr.Polymorphism;
 
-class Movie {
-    private String name;
-
-    public Movie(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String plot() {
-        return "No plot here";
-    }
-}
-
-class Jaws extends Movie {
-    public Jaws() {
-        super("Jaws");
-    }
-
-    public String plot() {
-        return "A shark eats people";
-    }
-}
-
-class IndependenceDay extends Movie {
-    public IndependenceDay() {
-        super("Independence Day");
-    }
-
-    @Override
-    public String plot() {
-        return "Aliens attempt to take over planet earth";
-    }
-}
-
-class MazeRunner extends Movie {
-    public MazeRunner() {
-        super("Maze Runner");
-    }
-
-    @Override
-    public String plot() {
-        return "Kids try and escape a maze.";
-    }
-}
-
-class Starwars extends Movie {
-    public Starwars() {
-        super("Star Wars");
-    }
-
-    @Override
-    public String plot() {
-        return "Imperial Forces try to take over the universe";
-    }
-}
-
-class ForgetableMovie extends Movie {
-    public ForgetableMovie() {
-        super("Forgetable");
-    }
-
-    // no plot method
-}
+import java.util.Scanner;
 
 public class Main
 {
     public static void main( String[] args ) {
-        car();
+        // car();
+
+        Movie theMovie = Movie.getMovie("Adventure", "Star Wars");
+        theMovie.watchMovie();
+        Scanner s = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter Type (A -> Adventure, C -> Comedy, S -> Science Fiction or Q -> Quit: ");
+            String type = s.nextLine();
+            if (type.contains("qQ"))
+                break;
+
+            System.out.println("Enter movie title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
     }
 
     public static void car() {
@@ -93,27 +43,5 @@ public class Main
         System.out.println(holden.startEngine());
         System.out.println(holden.accelerate());
         System.out.println(holden.brake());
-    }
-
-    public static void movie() {
-        for( int i = 1; i < 11; i++) {
-            Movie movie = randomMovie();
-            System.out.println("Movie #" + i + ": " + movie.getName() + "\n" + "Plot: " + movie.plot());
-        }
-    }
-
-    public static Movie randomMovie() {
-        int randomNumber = (int) (Math.random() * 5 + 1);
-        System.out.println("Random number generated was " + randomNumber);
-
-        return switch (randomNumber) {
-            case 1 -> new Jaws();
-            case 2 -> new IndependenceDay();
-            case 3 -> new MazeRunner();
-            case 4 -> new Starwars();
-            case 5 -> new ForgetableMovie();
-            default -> null;
-        };
-
     }
 }
