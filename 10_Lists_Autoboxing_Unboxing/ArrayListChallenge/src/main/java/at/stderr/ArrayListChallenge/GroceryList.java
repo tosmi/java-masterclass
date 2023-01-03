ff17ce6765e2837ab980eb7f4ea1e1ae723d8583
Toni Schmidbauer <toni@stderr.at>
2023-01-03 20:33:44 +0100

    work on arraylist challenge

---
+++ 10_Lists_Autoboxing_Unboxing/ArrayListChallenge/src/main/java/at/stderr/ArrayListChallenge/GroceryList.java
@@ -0,0 +1,15 @@
+package at.stderr.ArrayListChallenge;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<GroceryList> groceryList;
+
+
+    public void addItems() {
+    }
+
+    public void removeItems() {
+    }
+}
+
--- 10_Lists_Autoboxing_Unboxing/ArrayListChallenge/src/main/java/at/stderr/ArrayListChallenge/Main.java
+++ 10_Lists_Autoboxing_Unboxing/ArrayListChallenge/src/main/java/at/stderr/ArrayListChallenge/Main.java
@@ -1,12 +1,47 @@
 package at.stderr.ArrayListChallenge;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class Main 
 {
-    public static void main( String[] args ) {
 
+    public static void main( String[] args ) {
+        run();
+    }
+
+    public static void run() {
+        Scanner scanner = new Scanner(System.in);
+        boolean shutdown = false;
+        GroceryList groceryList = new GroceryList();
+
+        while(!shutdown) {
+            displayMenu();
+
+            var line = scanner.nextLine();
+            var input = Integer.parseInt(line);
+
+            switch (input) {
+                case 0 -> shutdown = true;
+                case 1 -> groceryList.addItems();
+                case 2 -> groceryList.removeItems();
+                default -> throw new IllegalStateException("Unexpected value: " + input);
+            }
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("""
+                Available actions:
+                
+                0 - to shutdown
+                1 - to add item(s) to list (comma delimited list)
+                2 - to remove item(s) (comma delimited list)
+
+                Enter a number for which action you want to do
+                """);
     }
 }
