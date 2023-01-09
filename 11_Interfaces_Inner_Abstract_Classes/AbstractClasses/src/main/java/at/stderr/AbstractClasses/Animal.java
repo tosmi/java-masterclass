a5ec06d3e1a3ccaf68e33e1f53aabda42f653b10
Toni Schmidbauer <toni@stderr.at>
2023-01-09 17:30:06 +0100

    import section 11

---
+++ 11_Interfaces_Inner_Abstract_Classes/AbstractClasses/src/main/java/at/stderr/AbstractClasses/Animal.java
@@ -0,0 +1,19 @@
+package at.stderr.AbstractClasses;
+
+import com.sun.source.tree.LabeledStatementTree;
+
+public abstract class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void eat();
+    public abstract void breathe();
+
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ 11_Interfaces_Inner_Abstract_Classes/AbstractClasses/src/main/java/at/stderr/AbstractClasses/Pinguin.java
@@ -0,0 +1,12 @@
+package at.stderr.AbstractClasses;
+
+public class Pinguin extends Bird {
+    public Pinguin(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I'm not very good at that");
+    }
+}
