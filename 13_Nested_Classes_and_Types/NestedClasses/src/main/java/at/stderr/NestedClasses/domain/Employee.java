409c1825636e38b928da9aac43e3a4525c4f060f
Toni Schmidbauer <toni@stderr.at>
2024-03-26 20:51:20 +0100

    work on nested classes

---
+++ 13_Nested_Classes_and_Types/NestedClasses/src/main/java/at/stderr/NestedClasses/domain/Employee.java
@@ -0,0 +1,25 @@
+package at.stderr.NestedClasses.domain;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private int yearStarted;
+
+    public Employee() {
+    }
+
+    public Employee(int employeeId, String name, int yearStarted) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.yearStarted = yearStarted;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "%d %s-8s %d".formatted(employeeId, name, yearStarted);
+    }
+}
---
+++ 13_Nested_Classes_and_Types/NestedClasses/src/main/java/at/stderr/NestedClasses/domain/EmployeeComparator.java
@@ -0,0 +1,11 @@
+package at.stderr.NestedClasses.domain;
+
+import java.util.Comparator;
+
+public class EmployeeComparator <T extends  Employee> implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return 0;
+    }
+}
