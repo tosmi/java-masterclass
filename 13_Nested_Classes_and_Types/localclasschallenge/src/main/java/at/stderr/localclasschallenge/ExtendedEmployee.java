526a359f04d9149e765a6e4927c96ecf0811b848
Toni Schmidbauer <toni@stderr.at>
2024-03-31 18:49:33 +0200

    section 13 done

--- 13_Nested_Classes_and_Types/localclasschallenge/src/main/java/at/stderr/localclasschallenge/ExtendedEmployee.java
+++ 13_Nested_Classes_and_Types/localclasschallenge/src/main/java/at/stderr/localclasschallenge/ExtendedEmployee.java
@@ -1,7 +1,6 @@
 package at.stderr.localclasschallenge;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public class ExtendedEmployee {
 
--- 13_Nested_Classes_and_Types/localclasschallenge/src/main/java/at/stderr/localclasschallenge/Main.java
+++ 13_Nested_Classes_and_Types/localclasschallenge/src/main/java/at/stderr/localclasschallenge/Main.java
@@ -2,8 +2,7 @@ package at.stderr.localclasschallenge;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -17,8 +16,58 @@ public class Main
     public static void main( String[] args ) {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee("John", "Doe", LocalDate.of(1999,12,10)));
+        employees.add(new Employee("JJ", "Abrams", LocalDate.of(1999,12,10)));
+        employees.add(new Employee("Max", "Mustermann", LocalDate.of(1997,06,10)));
+        employees.add(new Employee("Frank", "Zappa", LocalDate.of(2015,06,10)));
 
-        System.out.println("test");
+        printListOfEmployees(employees, "byName");
+        printListOfEmployees(employees, "byYear");
+    }
 
+    public static void printListOfEmployees(List<Employee> employees,String sortOrder) {
+        class ExtendedEmployee {
+            private Employee employee;
+            private String fullname;
+            int yearsHired;
+
+            public ExtendedEmployee(Employee employee) {
+                this.employee = employee;
+                //this.fullname = employee.firstName() + " " + employee.lastName();
+                this.fullname = String.join(" ",employee.firstName(), employee.lastName());
+                this.yearsHired = calculateYearsHired();
+            }
+
+            private int calculateYearsHired() {
+                int currentyear = LocalDate.now().getYear();
+                int hireyear = employee.hireDate().getYear();
+                return currentyear - hireyear;
+            }
+
+            @Override
+            public String toString() {
+                return "%-10s%-15s%-20s%-15d".formatted(employee.firstName(),employee.lastName(), fullname, yearsHired);
+            }
+        }
+
+        List<ExtendedEmployee> extendedEmployees = new ArrayList<>();
+        for (var e : employees) {
+            ExtendedEmployee extendedEmployee = new ExtendedEmployee(e);
+            extendedEmployees.add(extendedEmployee);
+        }
+
+        var comparator = new Comparator<ExtendedEmployee>() {
+            @Override
+            public int compare(ExtendedEmployee o1, ExtendedEmployee o2) {
+                return switch(sortOrder) {
+                    case "byYear" -> o1.employee.hireDate().getYear() - o2.employee.hireDate().getYear();
+                    default -> o1.employee.lastName().compareTo(o2.employee.lastName());
+                };
+            }
+        };
+
+        extendedEmployees.sort(comparator);
+        for (ExtendedEmployee ee : extendedEmployees) {
+            System.out.println(ee);
+        }
     }
 }
