9894c99d3b158f5b42ac387595a1e63f975abf03
Toni Schmidbauer <toni@stderr.at>
2024-06-21 20:42:57 +0200

    work on section 14

--- 14_Lambda/methodreferences/src/main/java/at/stderr/methodreferences/Main.java
+++ 14_Lambda/methodreferences/src/main/java/at/stderr/methodreferences/Main.java
@@ -3,8 +3,7 @@ package at.stderr.methodreferences;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 class PlainOld {
     private static int last_id = 1;
@@ -37,7 +36,31 @@ public class Main
         System.out.println("Getting array");
         PlainOld[] plainOldArray = seedArray(PlainOld::new, 10);
 
+//        calculator((s1, s2) -> s1.concat(s2), "Hello ", "World");
+        calculator(String::concat, "Hello ", "World");
 
+        BinaryOperator<String> b1 = (s1,s2) -> s1.concat(s2);
+        BiFunction<String, String, String> b2 = (s1,s2) -> s1.concat(s2);
+
+        // BinaryOperator<String> b1 = String::concat;
+        //BiFunction<String, String, String> b2 = String::concat;
+        //UnaryOperator<String> u1 = String::concat;
+        //UnaryOperator<String> u1 = (s1,s2) -> s1.concat(s2);
+        UnaryOperator<String> u1 = (s1) -> s1.toUpperCase();
+
+        System.out.println(b1.apply("Hello ", "World"));
+        System.out.println(b2.apply("Hello ", "World"));
+        System.out.println(u1.apply("Hello "));
+
+        String result = "Hello World".transform(u1);
+        System.out.println(result);
+
+        result = result.transform(String::toLowerCase);
+        System.out.println(result);
+
+        Function<String, Boolean> f0 = String::isEmpty;
+        boolean booleanResult = result.transform(f0);
+        System.out.println(booleanResult);
     }
 
     private static <T> void calculator(BinaryOperator<T> function, T value1, T value2) {
